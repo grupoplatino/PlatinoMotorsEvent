40d5ca3a85ecb9652a76b95f0ab4401f385773fb
taboradeyvi <dtabora@grupoplatino.hn>
2022-05-23 13:53:26 -0600

    Email js

--- src/components/body.js
+++ src/components/body.js
@@ -1,186 +1,168 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import { Card, Button, Container, Row, Col, Image, Modal, Form } from 'react-bootstrap'; // Stack,
-
+import React, { useRef, useState } from 'react';
+import { send } from 'emailjs-com';
+import { Button, Container, Row, Col, Image, Modal, Form } from 'react-bootstrap'; // Stack,
+import sany from '../assets/sany-logo.png'
+import logo from '../assets/logo-pm.png'
+// "homepage": "https://grupoplatino.github.io/PlatinoMotorsEvent/",
 
 const Body = () => {
-    const [modalShow, setModalShow] = React.useState(false);
+    const [show, setShow] = useState(false);
+    const [showErrModal, setShowErrModal] = useState(false);
 
-    return (
-        <Container fluid>
-            <Row fluid>
-                <Col xs="12" sm="12" md="6" >
-                    <Card className="text-center"
-                        style={{
-                            opacity: 3,
-                            // height: '90%',
-                            display: 'flex',
-                            marginTop: '15%',
-                            borderRadius: '25px',
-                            fontfamily: 'Popins'
-                        }}
-                    >
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
-                        <Card.Body >
-                            <Card.Title
-                                style={{
-                                    color: "black",
-                                    fontSize: 30,
-                                    marginTop: '1.5em',
-                                    fontWeight: "bolder",
-                                    fontfamily: "Josefin Sans Bungee Inline",
-                                }}
+    const handleCloseErrModal = () => setShowErrModal(false);
+    const handleShowErrModal = () => setShowErrModal(true);
 
-                            >Nos Complace Invitarle</Card.Title>
-                            <Card.Text fluid xs="12" sm="12" md="6">
-                                <h4 fluid xs="12" sm="12" md="6">
-                                    a la exposición de maquinaria pesada
-                                </h4>
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        from_email: '',
+        from_phone: '',
+    });
 
-                                <h1 fluid xs="12" sm="12" md="6"
-                                    style={{
-                                        color: "#F72F26",
-                                        fontSize: 50,
-                                        fontWeight: "bolder",
-
-                                    }}
-                                >
-                                    SANY</h1>
-                                <br />
-                                <h1 fluid xs="12" sm="12" md="6">Jueves 26 de Mayo</h1>
-                                <h2 fluid xs="12" sm="12" md="6">Hora: 4:00 pm en adelante</h2>
-                                <hr fluid xs="12" sm="12" md="6"
-                                    style={{
-                                        display: 'flex',
-                                        align: 'center',
-                                        height: '5px',
-                                        width: '100%',
-                                        color: 'black',
-                                        opacity: 0.6,
-                                        background: 'black',
-                                        borderColor: 'black',
-                                        borderRadius: '25px',
-                                    }}
-                                ></hr>
-                                <h1 fluid xs="12" sm="12" md="6">Lugar:</h1>
-                                <h2 fluid xs="12" sm="12" md="6">Anillo <b>periférico</b></h2>
-                                <br />
-                                <p fluid xs="12" sm="12" md="6"
-                                    style={{
-                                        fontSize: '25px',
-                                        fontWeight: 'normal',
-                                    }}
-                                >
-                                    Zona la cañada, <br />
-                                    frente a Dirstribuidora itsmania.
-                                </p>
-                            </Card.Text>
-                        </Card.Body>
-
-                        <Button variant="outline-danger" onClick={() => setModalShow(true)}
-                            style={{
-                                dispal: 'flex',
-                                height: '40px',
-                                background: "#F72F26",
-
-                            }}
-                        >
-                            <h5 fluid xs="12" sm="12" md="6"
-                                style={{
-                                    color: '#FFFFFF',
-                                }}
-                            >
-                                Confirmar
-                            </h5>
-
-                        </Button>
-
-                    </Card >
-                </Col>
-                <Col xs="12" sm="12" md="6">
-                    <Image fluid
-                        style={{
-                            width: '80%',
-                            // height: '95%',
-                            background: 'transparent',
-                            objectfit: 'contain',
-                            opacity: 3,
-                            marginTop: '15%',
-                            borderRadius: '25px',
-                        }}
-                        src='https://grupoplatino.blob.core.windows.net/develop/excavadora2.png' className='img-fluid shadow-4' alt='Sany' />
-                </Col>
-            </Row>
-            <ECVerticallyCenteredModal
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-
-            />
-        </Container >
-    );
-
-};
-function ECVerticallyCenteredModal(props) {
-    const form = useRef();
-    const sendEmail = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
-
-        emailjs.sendForm('service_ltp2k3h', 'template_qfzl4n1', form.current, 'user_rIgkpBy74HbPtpBcf5maa')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
+        send(
+            'service_ltp2k3h',
+            'template_qfzl4n1',
+            toSend,
+            'user_rIgkpBy74HbPtpBcf5maa'
+        )
+            .then((response) => {
+                setShow(false);
+                handleShowErrModal();
+            })
+            .catch((err) => {
+                console.log('FAILED...', err);
             });
     };
+
+    const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+    };
     return (
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Image fluid
-                    style={{
-                        with: '100%',
-                        height: '100px',
-                    }}
-                    src='https://grupoplatino.blob.core.windows.net/develop/logo-pm.png' />
-            </Modal.Header>
-            <Modal.Body>
-                <Form ref={Form} onSubmit={sendEmail}>
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Nombre</Form.Label>
-                        <Form.Control type="nombre" placeholder="Nombre: Ex. Juan Perez" />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Correo Electrónico</Form.Label>
-                        <Form.Control type="email" placeholder="Email: Ex. juanperez@email.com" />
-                        <Form.Text className="text-muted">
-                            Nosotros nunca compartiremos tu correo con nadie más.
-                        </Form.Text>
-                    </Form.Group>
+        <Container fluid id='card-invite' className='p-0'>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Teléfono</Form.Label>
-                        <Form.Control type="phonne" placeholder="Telefono" />
-                    </Form.Group>
-
+            <Modal
+                show={showErrModal}
+                onHide={handleCloseErrModal}
+                size="md"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered>
+                <Modal.Header closeButton>
+                    <Image fluid
+                        style={{
+                            with: '100%',
+                            height: '60px',
+                        }}
+                        src={logo} />
+                </Modal.Header>
+                <Modal.Body>
+                    <h5>Confirmación enviada correctamente.</h5>
+                    <p>Felicidades, tu confirmación de asistencia se ha enviado correctamente.</p>
                     <div className="d-flex flex-row-reverse">
-
-                        <Button variant="danger" onClick={props.on}>Confirmar Asistencia</Button>
-                        <Button style={{ marginRight: 10 }} variant="outline-danger" onClick={props.onHide}>Close</Button>
-
-
+                            <Button style={{ marginRight: 10 }} variant="outline-danger" onClick={handleCloseErrModal}>Cerrar</Button>
                     </div>
-                </Form>
-            </Modal.Body>
+                </Modal.Body>
+            </Modal>
 
-        </Modal>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                size="md"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered>
+                <Modal.Header closeButton>
+                    <Image fluid
+                        style={{
+                            with: '100%',
+                            height: '60px',
+                        }}
+                        src={logo} />
+                </Modal.Header>
+                <Modal.Body>
+                    <Form onSubmit={onSubmit}>
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Nombre*</Form.Label>
+                            <Form.Control name='from_name' required type="text" placeholder="Nombre: Ex. Juan Perez"
+                                onChange={handleChange} />
+                        </Form.Group>
 
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Correo Electrónico*</Form.Label>
+                            <Form.Control name='from_email' autoComplete='none' required type='email' placeholder="Email: Ex. juanperez@email.com"
+                                onChange={handleChange} />
+                            <Form.Text className="text-muted">
+                                Nosotros nunca compartiremos tu correo con nadie más.
+                            </Form.Text>
+                        </Form.Group>
 
-    )
-}
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Teléfono*</Form.Label>
+                            <Form.Control name='from_phone' required type="number" placeholder="Telefono: xxxxxxxx"
+                                onChange={handleChange} />
+                        </Form.Group>
+
+                        <div className="d-flex flex-row-reverse">
+                            <Button type='summit' variant="danger">Confirmar Asistencia</Button>
+                            <Button style={{ marginRight: 10 }} variant="outline-danger" onClick={handleClose}>Cerrar</Button>
+                        </div>
+                    </Form>
+                </Modal.Body>
+            </Modal>
+            <Container fluid className='bg-opacity px-0 pt-4'>
+                <Container fluid>
+                    <Row className='align-items-center'>
+                        <Col xs="12" sm="12" md="12" lg="7" >
+                            <div className="card text-center bg-transparent border-0">
+                                <div className='card-body'>
+                                    <div className='card-title head-one'
+                                        style={{ fontfamily: "Josefin Sans Bungee Inline" }}>
+                                        Nos Complace Invitarle
+                                    </div>
+                                    <div className='card-text'>
+                                        <h4>
+                                            a la exposición de maquinaria pesada
+                                        </h4>
+                                        <div className='mt-4'>
+                                            <img width={300} className='img-fluid' src={sany}></img>
+                                        </div>
+                                        <br />
+                                        <h1>Jueves 26 de Mayo</h1>
+                                        <h2>Hora: 4:00 pm en adelante</h2>
+                                        <hr className='hr-style'></hr>
+                                        <h1>Lugar:</h1>
+                                        <h2>Anillo <b>periférico</b></h2>
+                                        <p
+                                            style={{
+                                                fontSize: '25px',
+                                                fontWeight: 'normal',
+                                            }}
+                                        >
+                                            Zona la cañada, <br />
+                                            frente a Dirstribuidora itsmania.
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className='d-flex justify-content-center'>
+                                    <Button variant="outline-danger" onClick={handleShow}
+                                        className="w-50" style={{ background: "#F72F26" }}>
+                                        <h5 className='text-white'>Confirmar</h5>
+                                    </Button>
+                                </div>
+                            </div >
+                        </Col>
+                        <Col className='mt-5 mt-sm-5 mt-md-5 mt-lg-0' xs="12" sm="12" md="12" lg="5">
+                            <Image src='https://grupoplatino.blob.core.windows.net/develop/excavadora2.png'
+                                className='img-fluid shadow-4' alt='Sany' />
+                        </Col>
+                    </Row>
+                </Container >
+            </Container>
+        </Container>
+    );
+};
 
 
 export default Body;
--- src/components/footer.js
+++ src/components/footer.js
@@ -2,51 +2,45 @@
 import { Col, Row } from "react-bootstrap";
 import Container from 'react-bootstrap/Container';
 import { FaFacebookF, FaInstagram, FaGlobe } from "react-icons/fa";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const Footer = () => {
     return (
-        <Container fluid xs="12" sm="12" md="6"
+        <Container fluid
             style={{
                 alignItems: 'center',
             }}
         >
-            <hr
+            <hr className="m-0"
                 style={{
                     flex: 1,
-                    height: '10px',
+                    height: '5px',
                     opacity: 1,
                     borderRadius: '25px',
-                    // marginTop: '1em',
                     background: '#F72F26',
                     borderColor: '#F72F26',
                 }}
             ></hr>
-            <br />
-
-
-            <Row className="justify-content-center"
+            <Row className="justify-content-center mt-3"
                 style={{
                     spaceBetween: '10px',
                     fontfamily: 'Poppins',
-                    marginBottom: '20px',
-                }}
-            >
-                <Col xs md="auto"><FaGlobe color="black" /></Col>
-                <Col xs md="auto">
-                    <h5
+                }}>
+                <Col md="auto" className="p-0 d-none d-md-block"><FaGlobe color="black" /></Col>
+                <Col xs="12" md="auto">
+                    <h5 className="text-sm-left"
                         style={{
                             letterSpacing: '10px',
                             fontWeight: 'bold',
                         }}
                     >www.platino.hn </h5>
                 </Col>
-                <Col xs md="auto">|</Col>
-                <Col xs md="auto"><a href="https://www.facebook.com/platinomotors/" ><FaFacebookF color="black" /></a></Col>
-                <Col xs md="auto">|</Col>
-                <Col xs md="auto"><a href="https://instagram.com/platinomotors?igshid=YmMyMTA2M2Y=" ><FaInstagram color="black" /></a></Col>
-                <Col xs md="auto">|</Col>
-
+                <Col xs="2" md="auto">|</Col>
+                <Col xs="3" md="auto"><a href="https://www.facebook.com/platinomotors/" ><FaFacebookF color="black" /></a></Col>
+                <Col xs="2" md="auto">|</Col>
+                <Col xs="3" md="auto"><a href="https://instagram.com/platinomotors?igshid=YmMyMTA2M2Y=" ><FaInstagram color="black" /></a></Col>
+                <Col xs="2" md="auto">|</Col>
             </Row>
         </Container >
     );
